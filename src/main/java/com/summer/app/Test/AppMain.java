a917fd285c173d76c2eb085f51d4e67e14d3dc07
dirokim <dirokim7@gmail.com>
2023-12-22 12:48:27 +0900

    ad

---
+++ src/main/java/com/summer/app/Test/AppMain.java
@@ -0,0 +1,16 @@
+package com.summer.app.Test;
+
+import com.summer.app.util.DBConnector;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DBConnector connector = new DBConnector();
+		connector.getConnector();
+		
+		
+		
+	}
+
+}
