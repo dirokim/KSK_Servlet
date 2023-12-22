568d0d2e9197f81daef18609a2248e6067478f31
dirokim <dirokim7@gmail.com>
2023-12-22 12:05:50 +0900

    ad

---
+++ src/main/java/com/summer/app/util/DBConnector.java
@@ -0,0 +1,12 @@
+package com.summer.app.util;
+
+public class DBConnector {
+	//1. IP:PORT 
+	//2. ID,PW 
+	public void getConnector() {
+		String user="user01";
+		String password="user01";
+		String url="15.164.104.100:1521";
+		
+	}
+}
