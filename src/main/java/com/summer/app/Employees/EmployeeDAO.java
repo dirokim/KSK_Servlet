276e5cc3f657679f7c433d816add17be8af105f0
dirokim <dirokim7@gmail.com>
2023-12-26 09:51:34 +0900

    ad

--- src/main/java/com/summer/app/Employees/EmployeeDAO.java
+++ src/main/java/com/summer/app/Employees/EmployeeDAO.java
@@ -18,31 +18,31 @@ public class EmployeeDAO {
 		ResultSet rs = ps.executeQuery();
 		EmployeeDTO resultDTO = null;
 		if(rs.next()) {
-			resultDTO = new EmployeeDTO();
-			resultDTO.setEmployee_id(rs.getInt("EMPLOYEE_ID"));
-			resultDTO.setDepartment_id(rs.getInt("DEPARTMENT_ID"));
-			resultDTO.setLocation_id(rs.getInt("LOCATION_ID"));
-			resultDTO.setCountry_id(rs.getInt("COUNTRY_ID"));
-			resultDTO.setResion_id(rs.getInt("RESION_ID"));
+//			resultDTO = new EmployeeDTO();
+//			resultDTO.setEmployee_id(rs.getInt("EMPLOYEE_ID"));
+//			resultDTO.setDepartment_id(rs.getInt("DEPARTMENT_ID"));
+//			resultDTO.setLocation_id(rs.getInt("LOCATION_ID"));
+//			resultDTO.setCountry_id(rs.getInt("COUNTRY_ID"));
+//			resultDTO.setResion_id(rs.getInt("RESION_ID"));
+			employeeDTO.setEmployee_id(rs.getShort("FIRST_NAME"));
+		}else {
+			employeeDTO=null;
 			
 		}
+		
 		DBConnector.disConnect(rs, ps, con);
 		
-		return resultDTO;
+		return employeeDTO;
+		
 	}
 	
 	
 	public ArrayList<EmployeeDTO> getList() throws Exception {
 		Connection con = DBConnector.getConnector();
-		
 		String sql = "SELECT * FROM  EMPLOYEES";
-		
 		PreparedStatement ps = con.prepareStatement(sql);
-		
 		ResultSet rs = ps.executeQuery();
-		
 		ArrayList<EmployeeDTO> ar = new ArrayList<EmployeeDTO>();
-		
 		while(rs.next()) {
 			EmployeeDTO employeeDTO = new EmployeeDTO();
 			employeeDTO.setLocation_id(rs.getInt("LOCATION_ID"));
